e3f2cb928013873d0431e0000d64a1ae9b20b5e1
DigitalG <steamslonik2@ukr.net>
2018-05-07 22:26:31 +0300

    card init

---
+++ TBOL Card Game/Assets/Card.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+    private bool sticked_to_mouse = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (sticked_to_mouse)
+        {
+
+        }
+	}
+
+    void OnMouseDown()
+    {
+        if (sticked_to_mouse)
+            sticked_to_mouse = false;
+        else
+            sticked_to_mouse = true;
+    }
+}
